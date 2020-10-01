fd8b7bdc18a36f2c66d94cb6e50063f743b2eed9
innaropchan <inna.ropchan@gmail.com>
2020-10-01 19:36:41 +0200

    HW15

---
+++ HW_0110/src/de/telran/RepeatChar.java
@@ -0,0 +1,36 @@
+package de.telran;
+
+public class RepeatChar {
+    public static void main(String[] args) {
+
+        StringBuffer str = new StringBuffer("aaabbcdddd");
+        StringBuffer str1 = new StringBuffer("a3b2c1d4");
+
+
+        // System.out.println(str = str1);
+
+        System.out.println(printRepeatChar("aaabbcdddd"));
+
+    }
+
+    public static String printRepeatChar(String str) {
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        int d = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'a') {
+                a++;
+            } else if (str.charAt(i) == 'b') {
+                b++;
+            } else if (str.charAt(i) == 'c') {
+                c++;
+            } else if (str.charAt(i) == 'd') {
+                d++;
+            }
+        }
+        return "a" + a + "b" + b + "c" + c + "d" + ;
+    }
+
+}
+
