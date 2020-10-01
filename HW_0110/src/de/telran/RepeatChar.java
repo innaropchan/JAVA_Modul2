18bb9e26d50111f9ae657bacadc0bbe13bfd9f59
innaropchan <69244423+innaropchan@users.noreply.github.com>
2020-10-01 19:48:49 +0200

    Update RepeatChar.java
    https://github.com/LeSitar/TelRan_13M/issues/15

--- HW_0110/src/de/telran/RepeatChar.java
+++ HW_0110/src/de/telran/RepeatChar.java
@@ -29,7 +29,7 @@ public class RepeatChar {
                 d++;
             }
         }
-        return "a" + a + "b" + b + "c" + c + "d" + ;
+        return "a" + a + "b" + b + "c" + c + "d" + d;
     }
 
 }
