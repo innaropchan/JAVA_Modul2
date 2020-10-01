bdf79c58dfad925d2f36e774f03f2fd6f4fba2d1
innaropchan <inna.ropchan@gmail.com>
2020-10-01 07:56:30 +0200

    Date Validator

---
+++ 3009_DateValidator/src/de/telran/Main.java
@@ -0,0 +1,8 @@
+package de.telran;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(DateValidator.dateValidator(2019,2,29));
+        System.out.println(DateValidator.dateValidator(2020,2,29));
+    }
+}
